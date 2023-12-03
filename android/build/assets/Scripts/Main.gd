f4eb93e26d9212bc3875f159e25769781d36e14a
Henrik Weidner <henrik.weidner@hs-weingarten.de>
2023-12-03 15:47:16 +0100

    Add android/build/build/ to gitignore

--- android/build/assets/Scripts/Main.gd
+++ android/build/assets/Scripts/Main.gd
@@ -6,10 +6,12 @@ func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
 		print("OpenXR initialized successfully")
-
+		
 		# Turn off v-sync!
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-
+		
+		Engine.physics_ticks_per_second = 90
+		
 		# Change our main viewport to output to the HMD
 		get_viewport().use_xr = true
 	else:
