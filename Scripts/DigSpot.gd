51b7839eab963e52aad2ff9636599dbc98aa0f4c
Oliver Reisacher <oliver.reisacher@hs-weingarten.de>
2024-01-12 14:21:49 +0100

    Added Seed Snapping to Dig Spot. Added ReturnManager

--- Scripts/DigSpot.gd
+++ Scripts/DigSpot.gd
@@ -3,53 +3,69 @@ class_name DigSpot
 extends Node3D
 
 @export var remove_amount : float
+
+@export_category("Jiggel Settings")
 @export var max_jiggle : float
 @export var max_jiggle_speed : float
 
-@export var particles : PackedScene
+@export_category("Particles")
+@export var remove_particles : PackedScene
+@export var seed_insert_particles : PackedScene
+
+@onready var seed_snap_point : Node3D = $"Seed Snap Point"
 
 var anchor_cell : GridCell
 var cell_width : int
 
 # Removing function
 var progress : float
-var hand : Node3D
+var hand : XRToolsCollisionHand
 var previous_hand_position : Vector3
+
+# Seed
+var seed : Seed
 
 var time : float = 0.0
 var to_be_deleted : bool = false
 
 func _process(delta):
+	# Skip update if spot is currently in remove tween / animation
 	if to_be_deleted:
 		return
 	
+	# Remove jiggle timer
 	time += delta
 	
 	# Animate tile according to progress
 	if (progress > 0.0):
 		var ratio : float = progress / remove_amount
 		
+		# Jiggle depending on time and current ratio
 		var jiggle : float = sin(time * max_jiggle_speed) * max_jiggle * ratio
-		
 		rotation = Vector3(0, deg_to_rad(jiggle), 0)
 	
+	# Skip next steps if no player hand is currently here
 	if not hand:
 		return
 	
-	# Calc traveled distance
+	# Calculate traveled distance / new progress
 	var distance : float = hand.global_position.distance_to(previous_hand_position)
 	previous_hand_position = hand.global_position
 	
 	progress += distance
 	
+	# Check if "remove motion" is complete
 	if progress > remove_amount:
+		# Free occupied cells in grid
 		anchor_cell.grid.set_state(anchor_cell, cell_width, false)
 		
-		var parts : GPUParticles3D = particles.instantiate()
-		add_child(parts)
-		parts.position = Vector3(0, 0.2, 0)
-		parts.emitting = true
+		# Spawn remove particles
+		var particles : GPUParticles3D = remove_particles.instantiate()
+		add_child(particles)
+		particles.position = Vector3(0, 0.2, 0)
+		particles.emitting = true
 		
+		# Start remove animation
 		var remove_tween = create_tween()
 		remove_tween.tween_property(self, "global_position", global_position + Vector3(0, -0.2, 0), 2.0)
 		remove_tween.tween_callback(Callable(_free_callback))
@@ -60,13 +76,59 @@ func _free_callback():
 	queue_free()
 
 func _on_trigger_body_entered(body):
-	hand = body
+	# Check if body is hand
+	if body is XRToolsCollisionHand:
+		# Check if hand is currently holding a object
+		var function_pickup : XRToolsFunctionPickup = body.get_node("FunctionPickup")
+		
+		if not function_pickup or function_pickup.picked_up_object:
+			return
+		
+		# Save current hand
+		hand = body
+		previous_hand_position = body.global_position
 	
-	previous_hand_position = body.global_position
+	# Check if body is seed
+	if not seed and body is Seed:
+		seed = body
+		
+		# Force player to drop seed
+		seed.drop()
+		
+		# Disable pickable from seed
+		seed.enabled = false
+		seed.freeze = true
+		
+		# Reset rotation and place seed to snap point
+		var tween : Tween = create_tween()
+		tween.set_parallel(true)
+		tween.tween_property(seed, "rotation", Vector3.ZERO, 0.05)
+		tween.tween_property(seed, "global_position", seed_snap_point.global_position, 0.05)
+		
+		# Play jiggle animation for dig spot
+		tween.tween_property(self, "scale", Vector3(0.9, 0.9, 0.9), 0.05)
+		tween.chain().tween_property(self, "scale", Vector3(1.0, 1.0, 1.0), 0.05)
+		
+		# Spawn particles
+		var particles : GPUParticles3D = seed_insert_particles.instantiate()
+		add_child(particles)
+		particles.position = seed_snap_point.position + Vector3(0, 0.1, 0)
+		particles.emitting = true
+		
+		# Reparent seed after tween
+		tween.tween_callback(Callable(_reparent_seed_callback))
+
+func _reparent_seed_callback():
+	seed.get_parent().remove_child(seed)
+	seed_snap_point.add_child(seed)
+	
+	# Reset position to ZERO because of the reparenting
+	seed.position = Vector3.ZERO
 
 func _on_trigger_body_exited(body):
 	hand = null
+	time = 0.0
 	
+	# Lerp progress to zero
 	var tween = create_tween()
 	tween.tween_property(self, "progress", 0, 0.2)
-	# progress = 0.0
---
+++ Scripts/ReturnManager.gd
@@ -0,0 +1,87 @@
+@icon("res://Textures/ReturnManager.png")
+@tool
+class_name ReturnManager
+extends Area3D
+
+@export var group_name : String = "Returnable"
+@export var debug_draw : bool
+
+@export_category("Enable States")
+@export var enable_area_reset : bool = true
+@export var enable_range_reset : bool = true
+
+@export_category("Range Reset")
+@export var range : float = 1.0
+@export var range_origin : Vector3
+
+var reset_transforms = {}
+
+func _ready():
+	_get_data()
+
+func _process(delta):
+	# Draw debug lines if in editor
+	if debug_draw and Engine.is_editor_hint():
+		DebugDraw3D.draw_sphere(range_origin, range)
+	
+	if enable_range_reset and not Engine.is_editor_hint():
+		for node in reset_transforms:
+			var distance : float = node.global_position.distance_to(reset_transforms[node].origin)
+			
+			if distance > range:
+				# Tween scale / Animation
+				var tween : Tween = create_tween()
+				tween.tween_property(node, "scale", Vector3(0.1, 0.1, 0.1), 0.1)
+				tween.tween_callback(Callable(_restore_transform).bind(node))
+
+func _on_body_entered(body : RigidBody3D):
+	if not enable_area_reset:
+		return
+	
+	if body.is_in_group(group_name):
+		# Tween scale / Animation
+		var tween : Tween = create_tween()
+		tween.tween_property(body, "scale", Vector3(0.1, 0.1, 0.1), 0.1)
+		tween.tween_callback(Callable(_restore_transform).bind(body))
+
+func _restore_transform(node : RigidBody3D):
+	# Reset velocity of body
+	node.linear_velocity = Vector3.ZERO
+	node.angular_velocity = Vector3.ZERO
+	
+	# Reset transform (Position, Rotation)
+	node.transform = reset_transforms[node]
+
+func _get_data():
+	# Find all object in group "returnable"
+	var returnables : Array[Node] = get_tree().get_nodes_in_group(group_name)
+	
+	# Save object positions into dictonary for later usage
+	for node in returnables:
+		reset_transforms[node] = node.transform
+
+func update(clear_old : bool = true):
+	if clear_old:
+		# Clear all position
+		reset_transforms.clear()
+		
+		# Fill with new data
+		_get_data()
+	else:
+		# Find all object in group "returnable"
+		var returnables : Array[Node] = get_tree().get_nodes_in_group(group_name)
+		
+		# Remove old nodes
+		for node in reset_transforms:
+			if not returnables.has(node):
+				reset_transforms.erase(node)
+		
+		# Add new nodes
+		for node in returnables:
+			if not reset_transforms.has(node):
+				reset_transforms[node] = node.transform
+
+
+
+
+
