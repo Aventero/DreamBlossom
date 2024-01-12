1993b47fdb2c0b2c5d4c60758a4368f618a2e70b
Oliver Reisacher <oliver.reisacher@hs-weingarten.de>
2024-01-12 14:38:53 +0100

    Sorted files

--- Scripts/GridCell.gd
+++ Scripts/GridCell.gd
@@ -1,10 +1,13 @@
 class_name GridCell
 extends Resource
 
+# Grid in which cell exists
 var grid : PlantGrid
 
+# Index in grid
 var index : int
+# Position in grid
 var position : Vector2i
 
+# Current state
 var occupied : bool = false
-
